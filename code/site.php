3fd026f820f44814c5accda6fa33092b7a1c0b79
David <david.benavides@3wa.io>
2024-03-05 23:07:54 +0100

    Leanring variable

---
+++ code/site.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <?php
+        echo 'Hello World';
+
+        $human = array(
+            'sexe' => 'Male',
+            'age' => 25
+        );
+
+       echo '<pre>';   
+        var_dump($human);
+        echo '</pre>' ; 
+    ?>
+</body>
+</html>
